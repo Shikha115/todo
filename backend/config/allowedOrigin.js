af3378bf320cf6d5aefeb1002acb97e62168c63c
shikha <sj023279@gmail.com>
2025-11-09 22:30:41 +0530

    Add allowed origin for taskify-s4bt and refactor Vite config for consistency

--- backend/config/allowedOrigin.js
+++ backend/config/allowedOrigin.js
@@ -3,6 +3,7 @@ const allowedOrigin = [
   "http://localhost:5500",
   `http://localhost:${process.env.PORT}`,
   `https://taskify-backend-aw6w.onrender.com`,
+  `https://taskify-s4bt.onrender.com`,
 ];
 
 module.exports = allowedOrigin;
